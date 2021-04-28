dd635ff6d80e9bb858d8351aa00a9fdc56ef2281
hoody <rnmdo@163.com>
2021-04-28 13:14:15 +0800

    install element-plus,router,vuex ,configued both win and linux  building )

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-
+<div>
+  <img src="./assets/logo.png">
+  <div>
+    <p>
+      If Element Plus is successfully added to this project, you'll see an
+      <code v-text="'<el-button>'" />
+      below
+    </p>
+    <el-button type="primary">
+      el-button
+    </el-button>
+  </div>
   <HelloWorld msg="Welcome to Your Vue.js App" />
+</div>
 </template>
 
 <script>
